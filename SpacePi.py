21fd87718566cb8e72207231f8e8418e0e761e64
Henry O'Scannlain-Miller <Henryom@me.com>
2019-01-20 23:45:34 -0800

    automatic commit for deployment to pi

--- SpacePi.py
+++ SpacePi.py
@@ -9,18 +9,18 @@ cond = threading.Condition()
 notified = [False]
 
 def connectionListener(connected, info):
-    print(info, '; Connected=%s' % connected)
-    with cond:
-        notified[0] = True
-        cond.notify()
+	print(info, '; Connected=%s' % connected)
+	with cond:
+		notified[0] = True
+		cond.notify()
 
 NetworkTables.initialize(server='192.168.0.5')
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
 with cond:
-    print("Waiting")
-    if not notified[0]:
-        cond.wait()
+	print("Waiting")
+	if not notified[0]:
+		cond.wait()
 
 print("Connected!")
 
@@ -48,9 +48,9 @@ outputStream = cs.putVideo("Name", 320, 240)
 img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
 
 while True:
-    time, img = cvSink.grabFrame(img)
-    if time == 0:
-        outputStream.notifyError(cvSink.getError());
+	time, img = cvSink.grabFrame(img)
+	if time == 0:
+		outputStream.notifyError(cvSink.getError());
 		continue
 
 	hue_min = table.getNumber("hue_min", hue_min)
@@ -59,13 +59,13 @@ while True:
 	sat_max = table.getNumber("sat_max", sat_max)
 	val_min = table.getNumber("val_min", val_min)
 	val_max = table.getNumber("val_max", val_max)
- 	#img processing
- 	frame_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
- 	frame_binary = cv2.inRange(frame_hsv, (hue_min, sat_min, val_min), (hue_max, sat_max, val_max))
+	#img processing
+	frame_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	frame_binary = cv2.inRange(frame_hsv, (hue_min, sat_min, val_min), (hue_max, sat_max, val_max))
 
 
 
-    outputStream.putFrame(frame_binary)
+	outputStream.putFrame(frame_binary)
 
 
 

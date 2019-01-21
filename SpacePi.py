1dc6b82f287e32800215b355069ed3a91aa9d8dd
Henry O'Scannlain-Miller <Henryom@me.com>
2019-01-20 18:46:40 -0800

    testing

---
+++ SpacePi.py
@@ -0,0 +1,25 @@
+import cv2
+import threading
+from networktables import NetworkTables
+
+cond = threading.Condition()
+notified = [False]
+
+def connectionListener(connected, info):
+    print(info, '; Connected=%s' % connected)
+    with cond:
+        notified[0] = True
+        cond.notify()
+
+NetworkTables.initialize(server='192.168.0.5')
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+with cond:
+    print("Waiting")
+    if not notified[0]:
+        cond.wait()
+
+# Insert your processing code here
+print("Connected!")
+table = NetworkTables.getTable('SpaceVision')
+table.putNumber('ThisFingSucks', 1)
--- animation_test.py
+++ animation_test.py
@@ -6,14 +6,8 @@ An animated image
 This example demonstrates how to animate an image.
 """
 import numpy as np
-import cv2
-import threading
-from networktables import NetworkTables
-import random
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import copy
-
 
 fig = plt.figure()
 
@@ -24,11 +18,15 @@ def f(x, y):
 x = np.linspace(0, 2 * np.pi, 120)
 y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
 
-im = plt.imshow(cv2.imread("2016_images/0.jpg"), animated=True)
+im = plt.imshow(f(x, y), animated=True)
 
 
 def updatefig(*args):
-    return cv2.imread("2016_images/0.jpg")
+    global x, y
+    x += np.pi / 15.
+    y += np.pi / 20.
+    im.set_array(f(x, y))
+    return im,
 
 ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True)
 plt.show()

ba00d87f8f393301065abad918c5c7f2328d9e0b
Henry O'Scannlain-Miller <Henryom@me.com>
2019-01-21 00:20:37 -0800

    automatic commit for deployment to pi

--- setup_camera.sh
+++ setup_camera.sh
@@ -1,8 +1,9 @@
-v4l2-ctl -d /dev/video0 -c brightness=150
+v4l2-ctl -d /dev/video0 -c brightness=40
 v4l2-ctl -d /dev/video0 -c backlight_compensation=0
 v4l2-ctl -d /dev/video0 -c white_balance_temperature_auto=0
 v4l2-ctl -d /dev/video0 -c sharpness=50
 v4l2-ctl -d /dev/video0 -c saturation=50
 v4l2-ctl -d /dev/video0 -c contrast=0
-v4l2-ctl -d /dev/video0 -c exposure_absolute=0
 v4l2-ctl -d /dev/video0 -c exposure_auto=1
+v4l2-ctl -d /dev/video0 -c exposure_absolute=5
+
